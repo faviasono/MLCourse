506f32045a3ef0279f2edda78ea70f6416a9b4b9
Andrea Favia <37707874+faviasono@users.noreply.github.com>
2018-10-25 13:26:00 -0500

    Update gradientDescentMulti.m

--- machine-learning-ex1/ex1/gradientDescentMulti.m
+++ machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -19,21 +19,15 @@ for iter = 1:num_iters
 
 
 
-    
-
-
-
-
-
-
-
-    % ============================================================
-
-    % Save the cost J in every iteration    
     J_history(iter) = computeCostMulti(X, y, theta);
    
     delta = (1/m)*((X*theta-y)'*X)';
     theta = theta - alpha*delta;
+    
+
+    % ============================================================
+
+    % Save the cost J in every iteration    
     
 
 end

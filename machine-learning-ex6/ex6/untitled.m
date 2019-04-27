1d96ddc984bb94bb34ceaf59ecb271fb3c098daf
Andrea Favia <andrea.faviait@gmail.com>
2019-04-27 17:46:21 +0200

    Add files via upload

---
+++ machine-learning-ex6/ex6/untitled.m
@@ -0,0 +1,25 @@
+Gentile Prof. Cabodi,
+
+
+
+le scrivo per chiederle se fosse disposto a scrivere una lettera di raccomandazione per me. Ho seguito il corso di Algoritmi e Programmazione con lei e il professore Camurati(con valutazione pari a 30) durante il mio secondo anno al PoliTO.
+
+Nelle settimane successive dovro' iniziare le varie application procedures per il mio futuro master e, come puo' immaginare, la maggior parte delle universita' richiede almeno un contatto di un professore e una sua lettera di raccomandazione.
+
+
+
+Durante il mio secondo anno al PoliTo ho iniziato ad interessarmi al machine learning e a Data Science e, per questo motivo, vorrei cercare di entrare in alcune universita'in Europa(tra cui Edinburgh University, ETH, EPFL, UCL e TUM) al fine di poter acquire sostanziali conoscenze nel suddetto ambito e sviluppare nuove kills.
+
+Seppure probabilmente non e' l'ambito in cui lei fa ricerca, ho pensato di chiedere a lei questa lettera perche' il corso di APA mi ha fornito degli strumenti molto importanti ed utili che sono sicuro possano aiutarmi nel mio futuro accademico e lavorativo.
+
+
+
+Se ha piacere a scrivermi una lettera di raccomandazione, mi faccia sapere cosi'le posso dare piu' informazioni.
+
+La ringrazio in anticipo per il suo tempo e spero di sentirla presto.
+
+
+
+Saluti,
+
+Andrea Favia
